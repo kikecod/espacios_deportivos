c6a4f35bf40ab1d465a933e99fbbe535543cf593
Enrique Fernandez <151655928+kikecod@users.noreply.github.com>
2025-09-27 15:42:30 -0400

    Revert "Reserva pase acceso transaccion"

--- src/pases_acceso/dto/update-pases_acceso.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePasesAccesoDto } from './create-pases_acceso.dto';
-
-export class UpdatePasesAccesoDto extends PartialType(CreatePasesAccesoDto) {}
--- src/transacciones/dto/update-transaccione.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTransaccioneDto } from './create-transaccione.dto';
-
-export class UpdateTransaccioneDto extends PartialType(CreateTransaccioneDto) {}
