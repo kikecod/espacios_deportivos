99adad0b07e1effa17a001a8982426860191bd5f
kikecod <fernizenrique352@gmail.com>
2025-10-29 16:59:06 -0400

    se agrego para que se cree los roles al iniciar la base de datos

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseSeederService } from './database-seeder.service';
 import { Disciplina } from 'src/disciplina/entities/disciplina.entity';
+import { Rol } from 'src/roles/rol.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Disciplina])],
+  imports: [TypeOrmModule.forFeature([Disciplina, Rol])],
   providers: [DatabaseSeederService],
   exports: [DatabaseSeederService],
 })
