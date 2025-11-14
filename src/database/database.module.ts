8bd616b398e4c37683c5778ae99cf4b7d863b1d6
dgbau <god.caos300@gmail.com>
2025-11-13 23:43:13 -0400

    Seed administrador aun falta agregar funcionalidades

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,9 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseSeederService } from './database-seeder.service';
 import { Disciplina } from 'src/disciplina/entities/disciplina.entity';
 import { Rol } from 'src/roles/rol.entity';
+import { Persona } from 'src/personas/entities/personas.entity';
+import { Usuario } from 'src/usuarios/usuario.entity';
+import { UsuarioRol } from 'src/usuario_rol/entities/usuario_rol.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Disciplina, Rol])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Disciplina, 
+      Rol, 
+      Persona, 
+      Usuario, 
+      UsuarioRol
+    ])
+  ],
   providers: [DatabaseSeederService],
   exports: [DatabaseSeederService],
 })
